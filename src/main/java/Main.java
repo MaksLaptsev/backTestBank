259ac8b839408e293e2efd8c479dd9dde1ad1c3f
Max Laptsev <maksru2009@gmail.com>
2023-09-03 21:48:09 +0300

    feat: add Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import com.maksru2009.consoleApp.ConsoleApp;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        ConsoleApp consoleApp = new ConsoleApp();
+        consoleApp.startApp();
+
+    }
+}
